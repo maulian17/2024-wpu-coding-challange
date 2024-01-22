d78339ecb08a687f1faf91dde34d5b3abfe783ff
maulian17 <maulian.andita.edu@gmail.com>
2024-01-23 01:12:45 +0700

    update description for 1/366

--- 1-grow.js
+++ 1-grow.js
@@ -1,3 +1,7 @@
+//WPU Coding Challenge 2024
+// 1/366
+// https://www.codewars.com/kata/57f780909f7e8e3183000078
+
 // function grow(x) {
 //    let result = x[0];
 //    for (let i = 1; i < x.length; i++){
@@ -6,9 +10,6 @@
 //     return result;
 // }
 
-function grow(x) {
-  const result = x.reduce((acc, curr) => acc * curr, 1);
-  return result;
-}
+const grow = (x) => x.reduce((acc, curr) => acc * curr, 1);
 
 console.log(grow([2, 2, 2, 2, 2, 2]));
